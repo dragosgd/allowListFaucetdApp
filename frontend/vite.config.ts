1026506c0684f4af5f941790138a66c03515d17f
Dragos-Gabriel Danciulescu <dragos-gabrieldanciulescu@MacBook-Pro-tilhrende-Dragos-Gabriel.local>
2025-05-23 19:35:14 +0300

    Initial commit: Concordium Allow List + Faucet dApp

---
+++ frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@concordium/rust-bindings': '@concordium/rust-bindings/bundler',
+    }
+  },
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+  ],
+  define: {
+    global: 'globalThis',
+    'process.env': {
+      BACKEND_API: process.env.BACKEND_API
+    }
+  },
+});
