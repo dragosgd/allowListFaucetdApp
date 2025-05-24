98f99fbb1236605f11c2db74b6ec154e4dc2bcd8
Dragos-Gabriel Danciulescu <dragos-gabrieldanciulescu@MacBook-Pro-tilhrende-Dragos-Gabriel.local>
2025-05-24 14:36:22 +0300

    Confgure for env, update README FE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@concordium/rust-bindings': '@concordium/rust-bindings/bundler',
-    }
-  },
-  plugins: [
-    react(),
-    wasm(),
-    topLevelAwait(),
-  ],
-  define: {
-    global: 'globalThis',
-    'process.env': {
-      BACKEND_API: process.env.BACKEND_API
-    }
-  },
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    resolve: {
+      alias: {
+        '@concordium/rust-bindings': '@concordium/rust-bindings/bundler',
+      }
+    },
+    plugins: [
+      react(),
+      wasm(),
+      topLevelAwait(),
+    ],
+    define: {
+      global: 'globalThis',
+      'process.env': {
+        GOVERNANCE_ACCOUNT: `${env.GOVERNANCE_ACCOUNT}`,
+        TOKEN_ID: `${env.TOKEN_ID}`,
+        BACKEND_URL: `${env.BACKEND_URL}`
+      }
+    },
+  };
 });
